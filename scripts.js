cb906a39b3eaa69b45dac431ec386a85ddeedc77
victorariasdj <djvictor_arias@hotmail.com>
2024-07-31 14:42:01 -0500

    agregue boton de descarga para todos los mixes y sus landing pages, aplique logica de renombre y animacion de por favor espera

--- scripts.js
+++ scripts.js
@@ -5,26 +5,39 @@ menuIcon.addEventListener("click", () => {
     navLinks.classList.toggle("active");
 });
 
-document.getElementById('downloadBtn').addEventListener('click', function () {
-    const fileUrl = 'https://discoveryprovider.audius.co/v1/tracks/1Ex5Vg3/stream?app_name=EXAMPLEAPP';
-    
-    const fileName = 'Last Night A Dj Saved My Life (Victor Arias Remix).mp3';
-    
-    fetch(fileUrl)
-        .then(response => response.blob())
-        .then(blob => {
-            const url = URL.createObjectURL(blob);
+document.querySelectorAll('.downloadBtn').forEach(button => {
+    button.addEventListener('click', function () {
+        const fileUrl = this.getAttribute('data-url');
+        const fileName = this.getAttribute('data-filename');
 
-            const a = document.createElement('a');
-            a.href = url;
-            a.download = fileName;
-            document.body.appendChild(a);
-            a.click();
+        const notification = document.getElementById('notification');
+        const spinner = document.getElementById('spinner');
+        notification.style.display = 'block';
+        spinner.style.display = 'block';
 
-            setTimeout(() => {
-                document.body.removeChild(a);
-                URL.revokeObjectURL(url);
-            }, 0);
-        })
-        .catch(error => console.error('Error al descargar el archivo:', error));
+        fetch(fileUrl)
+            .then(response => response.blob())
+            .then(blob => {
+                const url = URL.createObjectURL(blob);
+
+                const a = document.createElement('a');
+                a.href = url;
+                a.download = fileName;
+                document.body.appendChild(a);
+                a.click();
+
+                notification.style.display = 'none';
+                spinner.style.display = 'none';
+
+                setTimeout(() => {
+                    document.body.removeChild(a);
+                    URL.revokeObjectURL(url);
+                }, 0);
+            })
+            .catch(error => {
+                console.error('Error al descargar el archivo:', error);
+                notification.style.display = 'none';
+                spinner.style.display = 'none';
+            });
+    });
 });
